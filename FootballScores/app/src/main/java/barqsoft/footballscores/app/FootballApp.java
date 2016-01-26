0ba8a0d9f7cf879274bfbdf84b7d174b1a6d57ad
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-01-27 00:21:14 +0530

    Add Application class

---
+++ FootballScores/app/src/main/java/barqsoft/footballscores/app/FootballApp.java
@@ -0,0 +1,31 @@
+package barqsoft.footballscores.app;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by Amrendra Kumar on 27/01/16.
+ */
+public class FootballApp extends Application {
+    private static FootballApp mApplication = null;
+
+    // should use static getInstance() method instead
+    public FootballApp() {
+        mApplication = this;
+    }
+
+    public static FootballApp getInstance() {
+        if (mApplication == null) {
+            mApplication = new FootballApp();
+        }
+        return mApplication;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+
+}
