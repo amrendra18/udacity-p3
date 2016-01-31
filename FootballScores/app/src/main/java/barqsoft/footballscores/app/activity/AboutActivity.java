76855e6b8fde72dce285daf28ca16fba2c48f09f
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-01-31 19:54:29 +0530

    Update about activity & fragment, Add toolbar to it

--- FootballScores/app/src/main/java/barqsoft/footballscores/app/activity/AboutActivity.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/app/activity/AboutActivity.java
@@ -2,7 +2,9 @@ package barqsoft.footballscores.app.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,17 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setIcon(R.drawable.ic_launcher);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(true);
+        }
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new AboutFragment())
